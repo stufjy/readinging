e25dab99b8138e34f039658e1e1b7084bbdf034b
stufjy <50316437+stufjy@users.noreply.github.com>
2019-07-04 20:51:48 +0800

    追加依赖库

--- cloudfunctions/booksearch/index.js
+++ cloudfunctions/booksearch/index.js
@@ -1,1 +1,16 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
 
+cloud.init()
+
+// 引入request-promise库
+var rp = require( 'request-promise' )
+// 云函数入口函数
+exports.main = async (event, context) => {
+  var url = `https://api.douban.com/v2/book/search?apikey=0df993c66c0c636e29ecbb5344252a4a&q=${event.myKeyID}&start=${event.start}&count=${event.count}`;
+  return rp( url ).then( res => {
+    return res;
+  }).catch( err => {
+    console.log( err )
+  })
+}
