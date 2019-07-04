751aca40b8096e5c174b2ce72efb00a013e618ca
stufjy <50316437+stufjy@users.noreply.github.com>
2019-07-04 20:47:15 +0800

    完善页面

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -1,18 +1,67 @@
 // pages/detail/detail.js
+// 初始化数据库
+const db = wx.cloud.database();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    bookID: '',  //存储书本ID
+    bookDetail: []  //保存书本数据
   },
 
+  // 跳转阅读界面
+  jumpreading: function(){
+    wx.navigateTo({
+      url: '/pages/reading/reading',
+    })
+  },
+  // 添加至集合books中
+  insert: function(){
+    db.collection( 'books' ).add({
+      data: {
+        book : this.data.bookDetail
+      }
+    }).then( res => {
+      // console.log( res )
+      wx.showToast({
+        title: '添加成功',
+      })
+    }).catch( err => {
+      console.log( err )
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // console.log( options.id );
+    this.setData({  
+      bookID: options.id   //将父组件传递过啦的bookid保存到bookID中
+    });
+    // 显示加载框
+    wx.showLoading({
+      title: '请求中',
+    });
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: "getDetail",  //云函数名称
+      data: {
+        bookKeyID: this.data.bookID  //传递的数据
+      }
+    }).then( res => {   //成功回调
+      var obj = JSON.parse(res.result)    //接收数据 将字符串转为对象
+      // console.log( obj )
+      this.setData({
+        bookDetail: obj   //将获取的数据 保存到bookDetail中
+      })
+      wx.hideLoading();  //隐藏加载框
+      console.log( this.data.bookDetail )
+    }).catch( err => {  //失败回调
+      console.log( err );
+      wx.hideLoading();   //隐藏加载框
+    })
   },
 
   /**
